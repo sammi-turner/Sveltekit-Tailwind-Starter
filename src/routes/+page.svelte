5787b4904b9ccf46053280e92ba22314fcd3dad3
Sammi Turner <ms.sammi.turner@gmail.com>
2023-07-23 16:43:28 +0100

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col items-center justify-center min-h-screen" style="background-color: #2c2c2c;">
+    <p class="text-3xl" style="color: #e0d2ce;">Welcome to Sveltekit</p>
+    <br>
+    <a href="https://kit.svelte.dev/docs/introduction" style="color: #f0440f">Click here to read the docs</a>
+  </div>
+  
