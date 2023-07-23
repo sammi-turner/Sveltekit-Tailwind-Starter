4be7be355bc35f5b7de6aec731b5820545aadf6c
Sammi Turner <ms.sammi.turner@gmail.com>
2023-07-23 20:02:53 +0100

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<div class="flex flex-col items-center justify-center min-h-screen" style="background-color: #2c2c2c;">
-    <p class="text-3xl" style="color: #e0d2ce;">Welcome to Sveltekit</p>
+<div class="flex flex-col items-center justify-center min-h-screen bg-svelte-brown">
+    <p class="text-3xl text-svelte-white">Welcome to Sveltekit</p>
     <br>
-    <a href="https://kit.svelte.dev/docs/introduction" style="color: #f0440f">Click here to read the docs</a>
+    <a class="text-svelte-orange" href="https://kit.svelte.dev/docs/introduction">Click here to read the docs</a>
   </div>
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    colors: {
+      'svelte-brown': '#2c2c2c',
+      'svelte-white': '#e0d2ce',
+      'svelte-orange': '#f0440f',
+    },
   },
   plugins: [],
 }
